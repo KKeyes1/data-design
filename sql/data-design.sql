49d0b607118a39a295d58eb772dd195f19fdb943
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-16 22:11:19 -0700

    tried to write 2 ea CRUD SQL commands

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -38,3 +38,32 @@ CREATE TABLE clap (
 	FOREIGN KEY(clapProfileId) REFERENCES profile(profileId),
 	PRIMARY KEY(clapArticleId, clapProfileId)
 );
+
+SELECT profileID
+FROM profile
+WHERE profileFullName = "Kenneth Keyes";
+
+SELECT articleDate, articleID, profileFullName
+FROM article
+INNER JOIN profile ON profile.profileID = article.articleAuthorProfileId
+WHERE articleDate = "August 2017";
+
+UPDATE profile
+SET profileCaption = "Is this thing on?"
+WHERE profileFullName = "Kenneth Keyes";
+
+UPDATE article
+SET articleContent = "h4ck 743 P14n37"
+WHERE articleAuthorProfileID = "7b3c750d-d6d2-40c2-9434-6ffe05639d89";
+
+DELETE FROM article
+WHERE articleDate = "August 2017";
+
+DELETE FROM profile
+WHERE profileId = "81af6286-2307-4bdc-8954-a7864e25435f";
+
+INSERT INTO profile(profileId, profileActivationToken, profileFullName, profileCaption, profileEmail, profileHash, profilePhone, profileSalt)
+VALUES ("b81d3bbd-59d8-4042-b0b3-5905c9227b07", "bhUPq3i8w90Kdv4QtwiT2cVk3YoLq", "Kenneth Keyes", "Ride like lightning, Crash like thunder", "foo@bar.com", "8743b52063cd84097a65d1633f5c74f5", "5052386850", "E1F53135E559C253");
+
+INSERT INTO article(articleId, articleAuthorProfileId, articleContent, articleDate)
+VALUE ("938a63aa-ee36-44df-9906-4f2d2439aca4", "b81d3bbd-59d8-4042-b0b3-5905c9227b07", "h4ck 743 P14n37", GETDATE());
