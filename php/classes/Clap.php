b100410b8ecebd82d8d48ff0817065402bf3338c
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-20 15:51:58 -0700

    set up clap class

--- php/classes/Clap.php
+++ php/classes/Clap.php
@@ -5,3 +5,24 @@ require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
+
+class Clap {
+	use ValidateDate;
+	use ValidateUuid;
+	/**
+	 * id of the article that this clap is for; this is a foreign key
+	 * @var Uuid $clapArticleId
+	 **/
+	private $clapArticleId;
+	/**
+	 * id of the Profile that sent this clap; this is a foreign key
+	 * @var Uuid $clapProfilId
+	 **/
+	private $clapProfileId;
+	/**
+	 * date and time this clap was sent, in a PHP DateTime object
+	 * @var \DateTime $clapDate
+	 **/
+	private $clapDate;
+
+}
