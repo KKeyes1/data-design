8aec2e998a5b5c57501dca88a63baba4733cda67
Kenneth Keyes <kkeyes1@cnm.edu>
2018-01-27 11:57:51 -0700

    fixed path to vendor

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -4,7 +4,7 @@ namespace Edu\Cnm\DataDesign;
 
 
 require_once("autoload.php");
-require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
